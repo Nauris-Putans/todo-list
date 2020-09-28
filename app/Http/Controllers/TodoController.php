1aeb58c4bc6fefb9bcfa48bf7acb0d490a25e5ff
Nauris Putans <nauris1122@gmail.com>
2020-09-28 11:24:22 +0300

    Added 'show' method for all dotos list.

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -10,7 +10,8 @@ class TodoController extends Controller
 {
     public function index()
     {
-        return view('todos.index');
+        $todos = Todo::all();
+        return view('todos.index', compact('todos'));
     }
 
     public function create()
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,1 +1,22 @@
-All todos
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-8 offset-2">
+
+            <div class="row">
+                <h1>All your To-Dos</h1>
+            </div>
+            
+            <ul>
+                @foreach($todos as $todo)
+                <li>
+                    {{$todo->title}}
+                </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+</div>
+@endsection
